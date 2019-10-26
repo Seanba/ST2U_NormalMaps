ec82afb982b8073594a1ca6e5a7dfc000ec2bd09
Seanba <sean@seanba.com>
2019-10-26 13:13:40 -0500

    Add SuperTiled2Unity to example

---
+++ Assets/SuperTiled2Unity/Scripts/Editor/Extensions/SuperMapExtensions.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace SuperTiled2Unity.Editor
+{
+    public static class SuperMapExtensions
+    {
+        public static Vector2 CellPositionToLocalPosition(this SuperMap superMap, int cx, int cy)
+        {
+            var grid = superMap.GetComponentInChildren<Grid>();
+            return grid.CellToLocal(new Vector3Int(cx, cy, 0));
+        }
+    }
+}
---
+++ Assets/SuperTiled2Unity/Scripts/Editor/StringConstants.cs
@@ -0,0 +1,13 @@
+﻿namespace SuperTiled2Unity.Editor
+{
+    public static class StringConstants
+    {
+        public const string Unity_Ignore = "unity:ignore";
+        public const string Unity_IsTrigger = "unity:IsTrigger";
+        public const string Unity_Layer = "unity:layer";
+        public const string Unity_SortingLayer = "unity:SortingLayer";
+        public const string Unity_SortingLayerName = "unity:SortingLayerName";
+        public const string Unity_SortingOrder = "unity:SortingOrder";
+        public const string Unity_Tag = "unity:tag";
+    }
+}

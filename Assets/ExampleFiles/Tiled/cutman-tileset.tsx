56bdb14b826539d3f7442fcaa3ca7a82509289eb
Seanba <sean@seanba.com>
2019-10-26 15:37:24 -0500

    Normal map working but importer not set up for it yet.

--- Assets/ExampleFiles/Tiled/cutman-tileset.tsx
+++ Assets/ExampleFiles/Tiled/cutman-tileset.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.4" name="cutman-level" tilewidth="16" tileheight="16" spacing="1" tilecount="49" columns="7">
+<tileset version="1.2" tiledversion="1.2.5" name="cutman-level" tilewidth="16" tileheight="16" spacing="3" margin="1" tilecount="49" columns="6">
  <image source="cutman-tileset.png" width="128" height="128"/>
  <tile id="0">
   <objectgroup draworder="index">
@@ -14,15 +14,15 @@
  <tile id="3">
   <objectgroup draworder="index"/>
  </tile>
- <tile id="7">
+ <tile id="6">
   <objectgroup draworder="index">
    <object id="1" x="0" y="0" width="16" height="16"/>
   </objectgroup>
  </tile>
- <tile id="10">
+ <tile id="9">
   <objectgroup draworder="index"/>
  </tile>
- <tile id="14">
+ <tile id="12">
   <objectgroup draworder="index">
    <object id="1" x="0" y="0" width="16" height="16"/>
   </objectgroup>
